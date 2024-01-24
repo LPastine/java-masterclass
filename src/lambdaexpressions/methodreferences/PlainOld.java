97a42af751d91b0cb8aabe2e4f0168d4b14ad8f9
LPastine <lucianopastine.tech@gmail.com>
2024-01-24 08:03:05 -0300

    method references

---
+++ src/lambdaexpressions/methodreferences/PlainOld.java
@@ -0,0 +1,10 @@
+package lambdaexpressions.methodreferences;
+
+public class PlainOld {
+    private static int last_id = 1;
+    private int id;
+    public PlainOld() {
+        id = PlainOld.last_id++;
+        System.out.println("Creating a PlainOld Object: id = " + id);
+    }
+}
