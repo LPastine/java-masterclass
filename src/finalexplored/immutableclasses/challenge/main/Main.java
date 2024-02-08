1475bbbf66d521b1b4d4793145da5d7da8fc0883
LPastine <lucianopastine.tech@gmail.com>
2024-02-08 08:54:50 -0300

    immutable classes challenge

---
+++ src/finalexplored/immutableclasses/challenge/main/Main.java
@@ -0,0 +1,20 @@
+package finalexplored.immutableclasses.challenge.main;
+
+import finalexplored.immutableclasses.challenge.bank.BankAccount;
+import finalexplored.immutableclasses.challenge.bank.BankCustomer;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+//        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING, 500);
+//        System.out.println(account);
+
+        BankCustomer joe = new BankCustomer("Joe", 500.00, 10000.00);
+        System.out.println(joe);
+
+        List<BankAccount> accounts = joe.getAccounts();
+        accounts.clear();
+        System.out.println(joe);
+    }
+}
