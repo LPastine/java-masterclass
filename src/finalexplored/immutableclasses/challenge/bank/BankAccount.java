6a506cab89a193627f8e65e86907c91e6901584e
LPastine <lucianopastine.tech@gmail.com>
2024-02-12 08:12:28 -0300

    unmodifiable collections challenge

--- src/finalexplored/immutableclasses/challenge/bank/BankAccount.java
+++ src/finalexplored/immutableclasses/challenge/bank/BankAccount.java
@@ -1,9 +1,15 @@
 package finalexplored.immutableclasses.challenge.bank;
 
+import finalexplored.immutableclasses.challenge.dto.Transaction;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class BankAccount {
     public enum AccountType {CHECKING, SAVINGS}
     private final AccountType accountType;
-    private final double balance;
+    private double balance;
+    private final Map<Long, Transaction> transactions = new LinkedHashMap<>();
 
     BankAccount(AccountType accountType, double balance) {
         this.accountType = accountType;
@@ -18,8 +24,22 @@ public class BankAccount {
         return balance;
     }
 
+    public Map<Long, String> getTransactions() {
+        Map<Long, String> txMap = new LinkedHashMap<>();
+        for (var tx : transactions.entrySet()) {
+            txMap.put(tx.getKey(), tx.getValue().toString());
+        }
+        return txMap;
+    }
+
     @Override
     public String toString() {
         return "%s $%.2f".formatted(accountType, balance);
+    }
+
+    void commitTransaction(int routingNumber, long transactionId, String customerId, double amount) {
+        balance += amount;
+        transactions.put(transactionId, new Transaction(routingNumber, transactionId,
+                Integer.parseInt(customerId), amount));
     }
 }
--- src/finalexplored/immutableclasses/challenge/main/Main.java
+++ src/finalexplored/immutableclasses/challenge/main/Main.java
@@ -1,7 +1,9 @@
 package finalexplored.immutableclasses.challenge.main;
 
+import finalexplored.immutableclasses.challenge.bank.Bank;
 import finalexplored.immutableclasses.challenge.bank.BankAccount;
 import finalexplored.immutableclasses.challenge.bank.BankCustomer;
+import finalexplored.immutableclasses.challenge.dto.Transaction;
 
 import java.util.List;
 
@@ -10,11 +12,42 @@ public class Main {
 //        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING, 500);
 //        System.out.println(account);
 
-        BankCustomer joe = new BankCustomer("Joe", 500.00, 10000.00);
+//        BankCustomer joe = new BankCustomer("Joe", 500.00, 10000.00);
+//        System.out.println(joe);
+
+        Bank bank = new Bank(3214567);
+        bank.addCustomer("Joe", 500.00, 10000.00);
+
+        BankCustomer joe = bank.getCustomer("000000010000000");
         System.out.println(joe);
 
-        List<BankAccount> accounts = joe.getAccounts();
-        accounts.clear();
-        System.out.println(joe);
+        if (bank.doTransaction(joe.getCustomerId(), BankAccount.AccountType.CHECKING, 35)) {
+            System.out.println(joe);
+        }
+
+        if (bank.doTransaction(joe.getCustomerId(), BankAccount.AccountType.CHECKING, -535)) {
+            System.out.println(joe);
+        }
+
+        BankAccount checking = joe.getAccount(BankAccount.AccountType.CHECKING);
+        var transactions = checking.getTransactions();
+        transactions.forEach((k, v) -> System.out.println(k + ": " + v));
+
+//        System.out.println("-------------------------------------------------");
+//
+//        for (var tx : transactions.values()) {
+//            tx.setCustomerId(2);
+//            tx.setAmount(10000.00);
+//        }
+//
+//        transactions.forEach((k, v) -> System.out.println(k + ": " + v));
+
+        joe.getAccount(BankAccount.AccountType.CHECKING).getTransactions().clear();
+
+        System.out.println("-------------------------------------------------");
+
+        joe.getAccount(BankAccount.AccountType.CHECKING).getTransactions()
+                .forEach((k, v) -> System.out.println(k + ": " + v));
+
     }
 }
