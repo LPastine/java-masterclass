9ab8161ec77d055ad18ec3bb61fa018e93029a0a
LPastine <lucianopastine.tech@gmail.com>
2024-05-17 08:13:59 -0300

    the structure of the stream pipeline

--- src/streams/Main.java
+++ src/streams/Main.java
@@ -40,17 +40,13 @@ public class Main {
         }
         System.out.println("-------------------------------------------");
 
-        bingoPool.stream()
+        var tempStream = bingoPool.stream()
                 .limit(15)
                 .filter(s -> s.indexOf('G') == 0 || s.indexOf("O") == 0)
                 .map(s -> s.charAt(0) + "-" + s.substring(1))
-                .sorted()
-                .forEach(s -> System.out.print(s + " "));
+                .sorted();
 
+        tempStream.forEach(s -> System.out.print(s + " "));
         System.out.println("\n----------------------------------------");
-
-        for (int i = 0; i < 15; i++) {
-            System.out.println(bingoPool.get(i));
-        }
     }
 }
