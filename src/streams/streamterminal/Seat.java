09547fd87ec70673e0c37100ea089d01589d4607
LPastine <lucianopastine.tech@gmail.com>
2024-05-20 06:12:50 -0300

    terminal operations

---
+++ src/streams/streamterminal/Seat.java
@@ -0,0 +1,10 @@
+package streams.streamterminal;
+
+import java.util.Random;
+
+public record Seat(char rowMaker, int seatNumber, boolean isReserved) {
+    public Seat(char rowMaker, int seatNumber) {
+//        this(rowMaker, seatNumber, new Random().nextBoolean());
+        this(rowMaker, seatNumber, false);
+    }
+}
