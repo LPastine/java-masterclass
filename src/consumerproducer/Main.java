201007dd4d9477edc1a8868feecfd5f3b0e37131
LPastine <lucianopastine.tech@gmail.com>
2024-06-06 07:54:12 -0300

    wait and notify

--- src/consumerproducer/Main.java
+++ src/consumerproducer/Main.java
@@ -5,9 +5,14 @@ import java.util.Random;
 class MessageRepository {
     private String message;
     private boolean hasMessage = false;
+    
     public synchronized String read() {
         while (!hasMessage) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         hasMessage = false;
@@ -16,9 +21,14 @@ class MessageRepository {
 
     public synchronized void write(String message) {
         while (hasMessage) {
-
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         hasMessage = true;
+        notifyAll();
         this.message = message;
     }
 }
