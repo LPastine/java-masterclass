79108f83fea54bcfebf282033fc37971c6dfc680
LPastine <lucianopastine.tech@gmail.com>
2024-06-20 07:23:53 -0300

    jpa in action

--- src/jpa/Main.java
+++ src/jpa/Main.java
@@ -1,7 +1,29 @@
 package jpa;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import jpa.music.Artist;
+
 public class Main {
     public static void main(String[] args) {
-
+        try (var sessionFactory =
+                     Persistence.createEntityManagerFactory(
+                             "dev.lpa.music"
+                     );
+             EntityManager entityManager = sessionFactory.createEntityManager();
+        ) {
+            var transaction = entityManager.getTransaction();
+            transaction.begin();
+//            entityManager.persist(new Artist("Muddy Water"));
+            Artist artist = entityManager.find(Artist.class, 202);
+//            Artist artist = new Artist(202, "Muddy Water");
+            artist.setArtistName("Muddy Waters");
+            System.out.println(artist);
+//            entityManager.remove(artist);
+//            entityManager.merge(artist);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
